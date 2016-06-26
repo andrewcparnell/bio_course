02152584154ce44ae9467b1f3479c98fc03acb1f
Andrew Parnell <andrew.parnell@gmail.com>
2016-06-26 14:39:57 +0100

    Updated code and data

---
+++ code/prostate_code.R
@@ -0,0 +1,23 @@
+# Code to run the Prostate cancer data set
+
+# Load in the data
+prostate = read.csv('prostate.csv')
+
+# Look at the first few rows:
+head(prostate)
+
+# Columns are:
+# lcavol - log(cancer volume)
+# lweight - log(weight)
+# age - age
+# lbph - log(benign prostatic hyperplasia amount)
+# svi - seminal vesicle invasion
+# lcp - log(capsular penetration)
+# gleason - grade of cancer
+# pgg45 - percentage Gleason scores 4 or 5
+# lpsa - outcome - log prostate specific antigen
+# train - whether the observation should be included in the training or test set
+# From Stamey et al (1989)
+
+# Create a plot
+pairs(prostate)
