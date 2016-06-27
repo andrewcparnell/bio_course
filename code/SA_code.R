393c6817f0b0d04d76968218f03f2d24445c9c39
Andrew Parnell <andrew.parnell@gmail.com>
2016-06-27 22:09:29 +0100

    Updated practicals and included Eugene code

--- code/SA_code.R
+++ code/SA_code.R
@@ -19,12 +19,16 @@ head(SA)
 # chd		response, coronary heart disease
 
 # Create a plot
-par(mfrow=c(2,2))
+par(mfrow=c(2,2)) # This command creates a 2 by 2 panel for plotting
 boxplot(age ~ chd, data = SA, xlab = 'chd', ylab = 'age')
 boxplot(tobacco ~ chd, data = SA, xlab = 'chd', ylab = 'tobacco')
 boxplot(alcohol ~ chd, data = SA, xlab = 'chd', ylab = 'alcohol')
 boxplot(obesity ~ chd, data = SA, xlab = 'chd', ylab = 'obesity')
-par(mfrow=c(1,1))
+par(mfrow=c(1,1)) # This command resets the plotting window
+
+## TASK: Try some other variables in the boxplots
+
+# Fit some classification models ------------------------------------------
 
 # Fit a model of chd vs age
 model_1 = glm(chd ~ age, data = SA, family = 'binomial')
@@ -33,4 +37,68 @@ summary(model_1)
 # Add an extra explanatory variable and an interaction
 model_2 = glm(chd ~ age + adiposity + age:adiposity, data = SA, family = 'binomial')
 summary(model_2)
+
+# Create a misclassification table for probability cut-off 0.5
+table(SA$chd, round(model_1$fitted.values), dnn=c('True','Predicted'))
+
+## QUESTION: What does the round function do? Why is the round statement equivalent to choosing a probability cut-off of 0.5?
+
+# Find another cut-off
+cut_off = 0.3
+# The next line contains some clever code, in particular the part as.integer(model_1$fitted.values>cut_off)
+# Try running these individual parts through the Console window to see what happens, e.g. just the part model_1$fitted.values, then just the part model_1$fitted.values>cut_off, then just the part as.integer(model_1$fitted.values>cut_off)
+tab = table(SA$chd, 
+            as.integer(model_1$fitted.values>cut_off), 
+            dnn=c('True','Predicted'))
+cat('Sensitivity = ',tab[2,2]/(tab[2,1] + tab[2,2]),
+    'Specificity = ',tab[1,1]/(tab[1,1] + tab[1,2]))
+
+## TASK: Try changing the cut off value and look at the effect on the sens and spec
+
+# Find the value that maximises Youden's index
+# Again this is more complicated code - work through it slowly
+prob_grid = seq(0.1, 0.6, length = 50) # Set up a sequence of probability values
+youden = rep(NA, length = 50) # Set up a storage container of same length
+for(i in 1:50) { # Loop through each probability sequence value
+  # Calculate the misclassification table
+  tab = table(SA$chd, 
+              as.integer(model_1$fitted.values>prob_grid[i]))
+  # Calculate sensitivity and specificity
+  sens = tab[2,2]/(tab[2,1] + tab[2,2])
+  spec = tab[1,1]/(tab[1,1] + tab[1,2])
+  # Calculate youden's index
+  youden[i] = sens + spec - 1
+}
+# Plot the output
+plot(prob_grid, youden, type = 'l')
+
+# TASK: An alternative to the youden index is the maximal efficiency metric defined as p*sens + (1-p)*spec. Implement this alternative metric and plot the output
+
+# Create the ROC curve
+library(pROC)
+plot(roc(SA$chd, model_1$fitted.values))
+
+## TASK: Try plotting the ROC curves of some of the other models. Can you see a difference? See if you can overplot the different models in different colours (hint: use add=TRUE)
+
+# Find the AUC value
+auc(SA$chd, model_1$fitted.values)
+
+## TASK: Try calculating the AUC values for different models. See if you can find a model with the highest AUC values
+
+# Calibration plot
+library(ROCR)
+pred = prediction(model_1$fitted.values, SA$chd)
+acc = performance(pred, measure = 'acc')
+plot(acc)
+abline(a=0, b=1, col='red')
+
+
+# Decision curve plot - not covered
+# library(DecisionCurve)
+# model_3 = decision_curve(chd ~ age,
+#                          data = SA, 
+#                          study.design = "cohort")
+# 
+# # plot the curve
+# plot_decision_curve(model_3,  curve.names = "CHD ~ age", cost.benefit.axis = FALSE)
 
--- code/prostate_code.R
+++ code/prostate_code.R
@@ -19,26 +19,91 @@ head(prostate)
 # train - whether the observation should be included in the training or test set
 # From Stamey et al (1989)
 
-# Create a plot
+# Create a matrix scatter plot
+# If this looks too small, click on the zoom button to get a bigger version
 pairs(prostate)
+
+## QUESTION: can you use square brackets to select only a few of the columns for the plot?
 
 # Look at the relationship between weight and lpsa
 par(mar=c(3,3,2,1), mgp=c(2,.7,0), tck=-.01,las=1)
-plot(prostate$lweight, prostate$lpsa, xlab = 'log(weight)', ylab= 'log(psa)')
+with(prostate, plot(lweight, lpsa, xlab = 'log(weight)', ylab= 'log(psa)'))
+
+# Run some regression models ----------------------------------------------
 
 # A first statistical model
 model_1 = lm(formula = lpsa ~ lweight, data = prostate)
 summary(model_1)
 
+## QUESTION: try changing the explanatory variable from lweight to lcavol. Does the model get better?
+
 # Two explanatory variables
 model_2 = lm(formula = lpsa ~ lweight + age, data = prostate)
 summary(model_2)
 
-# All of them - but get rid of training
+# Now use all of them - but get rid of training as it's not helpful
+# Note the format of the formula: response ~ explanatory + other variables - removed variables
+# The . means include all remaining variables
 model_3 = lm(formula = lpsa ~ . - train, data = prostate)
 summary(model_3)
+
+## QUESTION: which of the following three models do you think is best?
 
 # A model with interactions
 model_4 = lm(formula = lpsa ~ lweight + age + lweight:age, data = prostate)
 summary(model_4)
 
+## Task: Try some other interactions
+
+# Diagnostics -------------------------------------------------------------
+
+# Check the residuals:
+hist(model_1$residuals, freq=FALSE)
+curve(dnorm(x, sd = 1.046), col='red', add = TRUE)     
+
+## QUESTION: What does the curve function do? Try changing the colours
+
+# QQ plot
+qqnorm(model_1$residuals)
+qqline(model_1$residuals)
+
+# Predictions vs fits
+plot(prostate$lpsa, model_1$fitted.values,
+     ylab = 'Predicted values', xlab = 'True values')
+abline(a=0, b=1, col='red')
+# The abline function is useful for drawing straight lines
+
+## QUESTION: what do the a and b arguments of abline do?
+
+# 5-fold cross validation - this is more complicated code
+# It's worth spending some time trying to work out what's happening here
+# There are lots of new functions here so don't worry if you don't follow it all
+n_folds = 5 # Number of folds
+n = nrow(prostate) # Number of observations
+folds = sample(1:5, n, replace = TRUE) # Create the folds
+out_of_sample_preds = rep(NA, length = n) # Create a holder to store the output
+for(i in 1:n_folds) { # Loop through the folds
+  # Create a model by leaving out fold i
+  curr_model = lm(lpsa ~ lweight, data = subset(prostate, folds!=i))
+  # Predicted the values for fold i
+  out_of_sample_preds[folds==i] = predict(curr_model, newdata = subset(prostate, folds==i))
+}
+
+## TASK: Try changing the number of folds and check that the plot still runs.
+
+# Plot the out of sample predictions
+plot(prostate$lpsa, out_of_sample_preds,
+     ylab = '5-fold CV Predicted values', xlab = 'True values')
+abline(a=0, b=1, col='red')
+
+## QUESTION: Does the out of sample plot improve if you use lcavol instead of lweight?
+
+# Model comparison with AIC -----------------------------------------------
+
+# Model selection via stepAIC from the MASS package
+library(MASS)
+model_5 = stepAIC(model_4)
+summary(model_5)
+
+## TASK: see if you can follow the output from the stepAIC function
+## QUESTION: What happens if you start with a model that includes interactions?
