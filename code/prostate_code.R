dad782bd9e7fbc450e9ae3f87a6a109d31bd543a
Andrew Parnell <andrew.parnell@gmail.com>
2016-06-27 10:04:19 +0100

    Started class 2

--- code/prostate_code.R
+++ code/prostate_code.R
@@ -21,3 +21,20 @@ head(prostate)
 
 # Create a plot
 pairs(prostate)
+
+# Look at the relationship between weight and lpsa
+par(mar=c(3,3,2,1), mgp=c(2,.7,0), tck=-.01,las=1)
+plot(prostate$lweight, prostate$lpsa, xlab = 'log(weight)', ylab= 'log(psa)')
+
+# A first statistical model
+model_1 = lm(formula = lpsa ~ lweight, data = prostate)
+summary(model_1)
+
+# Two explanatory variables
+model_2 = lm(formula = lpsa ~ lweight + age, data = prostate)
+summary(model_2)
+
+# All of them
+model_3 = lm(formula = lpsa ~ ., data = prostate)
+summary(model_3)
+
